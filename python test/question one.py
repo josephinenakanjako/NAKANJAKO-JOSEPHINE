66ed675ce79d077d8f7af1965df59d29ed915f12
josephinenakanjako <143077466+josephinenakanjako@users.noreply.github.com>
2023-10-19 00:34:54 -0700

    Add files via upload

---
+++ python test/question one.py
@@ -0,0 +1,10 @@
+# part a
+students_marks=75
+studentsMarks=75
+
+#part b
+total_Average=60
+totalAverage=60
+
+age=22 
+print(type(age))
---
+++ python test/question3.py
@@ -0,0 +1,3 @@
+#sum of all items
+student_marks=[78,83,90,88,75]
+print(sum(student_marks))
